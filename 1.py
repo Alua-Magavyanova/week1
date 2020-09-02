1ce6127a9d8ef340c1e1954cc5ccf2223d0a88ad
Alua <70597885+Alua-Magavyanova@users.noreply.github.com>
2020-09-02 11:54:45 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+
+
+
+
+a=[    [4,  7, 8,  4, 3],
+       [4,  7, 8,  4, 3],
+       [4,  7, 8,  4, 3],
+       [4,  7, 8,  4, 3]
+       ]
+for i in range (len(a)):
+    
+    for j in range(len(a[i])):
+                    print(a[i][j], end="  ")
+    print()
